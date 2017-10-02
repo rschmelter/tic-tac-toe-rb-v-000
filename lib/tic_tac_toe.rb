745845a501ea99a526d47ef06213f316570e0f74
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-02 23:13:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,6 +24,16 @@ def input_to_index(input)
   input.to_i - 1
 end
 
+def turn_count(board)
+  counter = 0
+  board.each do |turn|
+      if turn == "X" || turn == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
 def current_player(board)
     if turn_count(board) % 2 == 0
       "X"
@@ -45,15 +55,6 @@ def valid_move?(board, index)
   index.between?(0, 8) && !position_taken?(board, index)
 end
 
-def turn_count(board)
-  counter = 0
-  board.each do |turn|
-      if turn == "X" || turn == "O"
-      counter += 1
-    end
-  end
-  counter
-end
 
 
 
