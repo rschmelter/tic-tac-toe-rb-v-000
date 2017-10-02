11b235a1e1c3674117a87d877da70816a9105f9e
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-02 23:02:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,6 +36,24 @@ def valid_move?(board, index)
   index.between?(0, 8) && !position_taken?(board, index)
 end
 
+def turn_count(board)
+  counter = 0
+  board.each do |turn|
+      if turn == "X" || turn == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+    if turn_count(board) % 2 == 0
+      "X"
+    else
+      "O"
+  end
+end
+
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
@@ -56,25 +74,9 @@ end
 # end
 # end
 
-def turn_count(board)
-  counter = 0
-  board.each do |turn|
-      if turn == "X" || turn == "O"
-      counter += 1
-    end
-  end
-  counter
-end
 
-def current_player(board)
-    if turn_count(board) % 2 == 0
-      "X"
-    else
-      "O"
-  end
-end
 
-token = current_player(board)
+
 
 
 def won?(board)
